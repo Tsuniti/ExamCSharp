453dcba8349e78adacb620963d037c158b8337c0
Tsuniti <Tsuniti00@gmail.com>
2024-01-27 10:46:39 +0200

    creating class Character.cs and adding Character.cs in ApplicationContext.cs

--- ApplicationContext.cs
+++ ApplicationContext.cs
@@ -4,6 +4,7 @@ using Microsoft.EntityFrameworkCore;
 public class ApplicationContext : DbContext
 {
     public DbSet<User> Users => Set<User>();
+    public DbSet<Character> Characters => Set<Character>();
     public ApplicationContext() => Database.EnsureCreated();
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
---
+++ Character.cs
@@ -0,0 +1,9 @@
+﻿namespace ExamCSharp;
+
+public class Character
+{
+    public int Id { get; set; }
+    public string NickName { get; set; }
+    public int Level { get; set; }
+    public int UserId { get; set; }
+}
