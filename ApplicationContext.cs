73721c0495d92190a14a8cf6c355a6eec4fae5d1
Tsuniti <Tsuniti00@gmail.com>
2024-01-27 09:59:16 +0200

    creating AplicationContext.cs

---
+++ ApplicationContext.cs
@@ -0,0 +1,13 @@
+﻿using ExamCSharp;
+using Microsoft.EntityFrameworkCore;
+
+public class ApplicationContext : DbContext
+{
+    public DbSet<User> Users => Set<User>();
+    public ApplicationContext() => Database.EnsureCreated();
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite("Data Source=game.db");
+    }
+}
